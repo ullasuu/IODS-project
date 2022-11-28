48df3c4e9f5ad1522f745a347607b6453d5fc809
Suulamo <usuulamo@ad.helsinki.fi>
2022-11-28 22:08:37 +0200

    small fix

--- data/create_human.R
+++ data/create_human.R
@@ -3,26 +3,19 @@
 # IODS Assignment 4. Data wrangling for week 5 assignment
 
 setwd("C:/Users/usuulamo/Introduction to Open Data Science PHD-302/IODS-project")
-
 library(tidyverse)
 
-
 # Read in “Human development” and “Gender inequality” data sets
-
 hd <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/human_development.csv")
 gii <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/gender_inequality.csv", na = "..")
 
 # Structure and dimensions
 str(hd); dim(hd) # 195 observations of 8 variables
 str(gii); dim(gii) # 195 observations of 10 variables
-
-
 summary(hd)
 summary(gii)
 
-
 # Rename
-
 hd <- hd %>% 
   rename(
     hdi_rank = `HDI Rank`,
@@ -49,23 +42,16 @@ gii <- gii %>%
     labo_m = `Labour Force Participation Rate (Male)`
   )
 
-
 # New variables
-
 gii <- gii %>%
   mutate(
     edusec_fm = edusec_f / edusec_m,
     labo_fm = labo_f / labo_m
   )
 
-head(gii)
-
-
 # Join datasets
-
 human <- inner_join(hd, gii, by="country")
 dim(human) # 195 observations of 19 variables
-
 
 # Save
 write_csv(human, "data/human.csv")
